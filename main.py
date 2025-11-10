d0d64177945e6812a46e3937e64caab12b35e237
Emilio Orozco <eaorozco3@miners.utep.edu>
2025-11-10 16:05:02 -0700

    Called cleanup and printed updated dataframe.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import sklearn.metrics.pairwise as kernel_lib
+from dataset import clean_dataset
 
 if __name__ == "__main__":
     df = pd.read_csv('healthcare-dataset-stroke-data.csv')
-    print(df)
+    df_updated = clean_dataset(df)
+    print(df_updated)
