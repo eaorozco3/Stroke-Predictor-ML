834af12c8aa343c73088b6a800543997fa50ecf6
Emilio Orozco <eaorozco3@miners.utep.edu>
2025-11-10 16:04:28 -0700

    Added functionality to replace N/A BMIs with the average.

--- dataset.py
+++ dataset.py
@@ -2,6 +2,6 @@ def clean_dataset(df):
     df_updated = df.copy()
 
     df_updated = df_updated.drop("id", axis = 1)
-    df_updated["bmi"] = df_updated["bmi"].fillna(0)
+    df_updated["bmi"] = df_updated["bmi"].fillna(df_updated["bmi"].mean())
 
     return df_updated
