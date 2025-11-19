bf54d77f5c60ef9bcccc63cc2f0a1bc1fa96fbfd
Emilio Orozco <eaorozco3@miners.utep.edu>
2025-11-19 15:38:12 -0700

    Added scaled perceptron.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 import sklearn.metrics.pairwise as kernel_lib
 from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.model_selection import train_test_split
@@ -16,9 +17,15 @@ if __name__ == "__main__":
     x_numerical = categorical_to_numerical(x.copy())
 
     X_train, X_test, y_train, y_test = train_test_split(x_numerical, y, test_size=0.2, random_state=42, stratify=y)
-    #print(x_numerical)
-    regular_perceptron = regular_perceptron(X_train, y_train)
-    y_pred = regular_perceptron.predict(X_test)
-    print("Regular Perceptron Results:")
+    r_perceptron = regular_perceptron(X_train, y_train)
+    y_pred = r_perceptron.predict(X_test)
+    print("\nRegular Perceptron Results:")
     print(classification_report(y_test, y_pred))
 
+    scaler = StandardScaler()
+    x_train_scaled = scaler.fit_transform(X_train)
+    x_test_scaled = scaler.transform(X_test)
+    scaled_perceptron = regular_perceptron(x_train_scaled, y_train)
+    y_scaled_pred = scaled_perceptron.predict(x_test_scaled)
+    print("\nScaled Perceptron Results:")
+    print(classification_report(y_test, y_scaled_pred))
