be11f9f8a4cbe32738077fec3b93770988ef770d
Emilio Orozco <eaorozco3@miners.utep.edu>
2025-11-19 15:41:12 -0700

    Added random forest implementation.

---
+++ random_forest.py
@@ -0,0 +1,5 @@
+from sklearn.ensemble import RandomForestClassifier
+
+def generate_rf(x_train, y_train):
+    random_forest = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced')
+    random_forest.fit(x_train, y_train)
